ff5df8c3448455d7663f1174c61ba091fede42f1
kitasota <kitasota123@gmail.com>
2020-08-29 17:20:19 +0900

    night-cafe

--- js/sample.js
+++ js/sample.js
@@ -218,8 +218,50 @@ $(function(){
       focusOnSelect: true,
       slidesToShow:4,
       slidesToScroll:1
-  });    
-
+  }); 
+  
+   
 });
 
+$(function() {
+   //（１）ページの概念・初期ページを設定
+   var page=0;
+    
+   //（２）イメージの数を最後のページ数として変数化
+   var lastPage =parseInt($("#slide3 img").length-1);
+   
+   //（３）最初に全部のイメージを一旦非表示にします
+        $("#slide3 img").css("display","none");
+   
+   //（４）初期ページを表示
+             $("#slide3 img").eq(page).css("display","block");
+   
+   //（５）ページ切換用、自作関数作成
+   function changePage(){
+                            $("#slide3 img").fadeOut(1000);
+                            $("#slide3 img").eq(page).fadeIn(1000);
+   };
+   
+   //（６）～秒間隔でイメージ切換の発火設定
+   var Timer;
+   function startTimer(){
+   Timer =setInterval(function(){
+             if(page === lastPage){
+                            page = 0;
+                            changePage();
+                  }else{
+                            page ++;
+                            changePage();
+             };
+        },2500);
+   }
+   
+   //（７）～秒間隔でイメージ切換の停止設定
+   function stopTimer(){
+   clearInterval(Timer);
+   }
+   
+   //（８）タイマースタート
+   startTimer();
+  });
 
